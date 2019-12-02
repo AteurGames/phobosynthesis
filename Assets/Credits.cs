9a27bd09eddd505bdca892832db37c5b5d1fb48b
AteurGames <unconfigured@null.spigotmc.org>
2019-12-02 15:52:19 -0800

    pototype 1.0

---
+++ Assets/Credits.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Credits : MonoBehaviour {
+    //Flashlight sound effect: http://soundbible.com/1970-Pen-Clicks.html "Pen Clicks Sound" by Simon Craggs under CC Attribution 3.0
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/MenuUi.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUi : MonoBehaviour {
+    public void Begin() {
+        SceneManager.LoadSceneAsync("Level1");
+    }
+    public void Quit() {
+        Application.Quit();
+    }
+}
